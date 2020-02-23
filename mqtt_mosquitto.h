07f66995d6b77e7cddd613986711cd33fea5c42e
churruscat <luis.morras@gmail.com>
2020-02-23 11:22:02 +0200

    Add files via upload

---
+++ mqtt_mosquitto.h
@@ -0,0 +1,33 @@
+
+#define MQTT_MAX_PACKET_SIZE 455 //cambialo antes de incluir docpatth\Arduino\libraries\pubsubclient-master\src\pubsubclient.h
+#define MQTT_KEEP_ALIVE 60
+#include <PubSubClient.h> // https://github.com/knolleary/pubsubclient/releases/tag/v2.3
+#include <ArduinoJson.h> // https://github.com/bblanchon/ArduinoJson/releases/tag/v5.0.7
+
+/*************************************************
+ ** -------- Valores Personalizados ----------- **
+ * ***********************************************/
+#define DEVICE_TYPE "ESP32"
+#define ORG "canMorras"
+#define DEVICE_ID "M_Oscura_1"
+char* ssid;
+char* password;
+char ssid1[] = "Materia_Oscura";
+char password1[] ="Viva1la2Vela";
+char ssid2[] ="Materia_Oscura";
+char password2[] = "Viva1la2Vela";
+/*************************************************
+ ** ----- Fin de Valores Personalizados ------- **
+ * ***********************************************/
+#define ESPERA_NOCONEX 60000  // cuando no hay conexion, descanso un minuto
+char server[] = "192.168.10.1";
+char * authMethod = NULL;
+char * token = NULL;
+char clientId[] = "d:" ORG ":" DEVICE_TYPE ":" DEVICE_ID;
+
+char publishTopicMeteo[] = "meteo/envia" ;  // el dispositivo envia datos a Mosquitto
+char publishTopicBateria[] = "bateria/envia";  // el dispositivo envia datos a Mosquitto
+char metadataTopic[]= "bateria/envia/metadata/" DEVICE_ID; //el dispositivo envia sus metadatos a Mosquitto
+char updateTopic[]  = "bateria/update/" DEVICE_ID;    // Mosquitto o node-red me actualiza los metadatos
+char responseTopic[]= "bateria/response/" DEVICE_ID;
+char rebootTopic[]  = "bateria/reboot/" DEVICE_ID;
